e60988e1b1b0cea46190e7fa657c3195330bd542
foggy_hedgehog <foggy80hedgehog@gmail.com>
2023-02-14 11:58:40 +0300

    Создал папку Task

---
+++ Tast/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3.
+Первоночальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+*/
+
+Console.Clear();
+Console.WriteLine("Введите длинну массива ");
+int N = Convert.ToInt32(Console.ReadLine());
+string[] MassivFirst = new string[N];
+
+for (int i = 0; i < N; i++)
+{
+    Console.WriteLine($"Введите Значение {N - 1} в массив :");
+    MassivFirst[i] = Console.ReadLine();
+}
+
+
+
